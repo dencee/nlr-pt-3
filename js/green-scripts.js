9528de60959d047949eef2201451e2569a02009b
Daniel Commins <daniel.commins@techelevator.com>
2023-09-26 18:57:53 -0700

    add green class fan pages

--- js/green-scripts.js
+++ js/green-scripts.js
@@ -5,7 +5,15 @@ const classInfo = [];
 /*
  * Add fan pages below
  */
-classInfo.push({ name: 'A. Developer', fanPageUrl: "https://en.wikipedia.org/wiki/Green" });
+classInfo.push({ name: 'Steve Her', fanPageUrl: 'https://sth84478.github.io/fan-page/' });
+classInfo.push({ name: 'Stephen Green', fanPageUrl: 'https://stevegreen4.github.io/fan-page/' });
+classInfo.push({ name: 'Samantha Stinchcomb', fanPageUrl: 'https://thepudgypigeon.github.io/fan-page/' });
+classInfo.push({ name: 'Josh Hurley', fanPageUrl: 'https://jrhurleycode.github.io/fan-page/' });
+classInfo.push({ name: 'Joel Rennert', fanPageUrl: 'https://joelrennert.github.io/fan-page/' });
+classInfo.push({ name: 'Jason Scott', fanPageUrl: 'https://jasondscott90.github.io/fan-page/' });
+classInfo.push({ name: 'Heidi Jones', fanPageUrl: 'https://heid9107.github.io/fan-page/' });
+classInfo.push({ name: 'Emmy Curry', fanPageUrl: 'https://emmycurry.github.io/fan-page/' });
+classInfo.push({ name: 'Daniel Lopez', fanPageUrl: 'https://dubdayn.github.io/lucy-fanPage/' });
 
 window.addEventListener('DOMContentLoaded', event => {
 
