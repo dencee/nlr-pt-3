909c5a4869d8e91467751ecabad22138ac6da30e
Space-Scout <benmayf343@gmail.com>
2023-10-05 02:59:48 -0400

    Initial Spring Boot download and commit

---
+++ classes/orange/orange-login/orange-login/src/main/java/com/orangefanpage/orangelogin/OrangeLoginApplication.java
@@ -0,0 +1,13 @@
+package com.orangefanpage.orangelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeLoginApplication.class, args);
+	}
+
+}
---
+++ classes/orange/orange-login/orange-login/src/test/java/com/orangefanpage/orangelogin/OrangeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangefanpage.orangelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
