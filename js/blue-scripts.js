96cae117139e7e775538068256a34de8546f03a5
Jerry Balderas <balderasfam@gmail.com>
2023-10-06 21:18:21 -0500

    Adding site for Chris

--- js/blue-scripts.js
+++ js/blue-scripts.js
@@ -7,6 +7,7 @@ const classInfo = [];
  */
 classInfo.push({ name: 'Hannah Schomaker', fanPageUrl: "https://schomakerh14.github.io/schomakerh14.github.io-marceline-fan-page/" });
 classInfo.push({ name: 'Steven Diamantopoulos', fanPageUrl: "https://stavroz96.github.io/starcraft2-demo/" });
+classInfo.push({ name: 'Chris Ratsabout', fanPageUrl: "https://chrisratsabout.github.io/te-html-fan-page/" });
 
 
 window.addEventListener('DOMContentLoaded', event => {
